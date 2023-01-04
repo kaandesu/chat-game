ad402951f0ec214d1b1e727eda6a7c1a8620bdfb
kaandesu <kaandesu@users.noreply.github.com>
2023-01-04 21:46:37 +0000

    deploy: 60353f05f719dcc9a19330a5d66797d2f56a1077

--- src/components/Chat/chat-components.js
+++
@@ -1,4 +0,0 @@
-export {default as Footer} from './ChatFooter.vue';
-export {default as Header} from './ChatHeader.vue';
-export {default as Section} from './ChatSection.vue';
-export {default as Message} from './ChatMessage.vue';
--- src/components/scenarios/chat-scenarios.js
+++
@@ -1,2 +0,0 @@
-export {default as ChatOne} from './chat1.js';
-export {default as ChatTwo} from './chat2.js';
